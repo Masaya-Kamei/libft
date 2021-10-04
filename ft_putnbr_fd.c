bd57301c91c024b7f4fa5aa51bd40e5e2656b07e
mkamei <mkamei@student.42tokyo.jp>
2021-10-04 19:25:45 +0900

    refactaring

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,16 +6,16 @@
 /*   By: mkamei <mkamei@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/10 11:56:26 by mkamei            #+#    #+#             */
-/*   Updated: 2021/05/27 18:32:39 by mkamei           ###   ########.fr       */
+/*   Updated: 2021/10/04 18:00:07 by mkamei           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static void	print_nbr(unsigned int nbr, int fd)
+static void	rec_write_one_digit(unsigned int nbr, int fd)
 {
 	if (nbr >= 10)
-		print_nbr(nbr / 10, fd);
+		rec_write_one_digit(nbr / 10, fd);
 	ft_putchar_fd(nbr % 10 + '0', fd);
 }
 
@@ -30,5 +30,5 @@ void	ft_putnbr_fd(int n, int fd)
 	}
 	else
 		un = n;
-	print_nbr(un, fd);
+	rec_write_one_digit(un, fd);
 }
